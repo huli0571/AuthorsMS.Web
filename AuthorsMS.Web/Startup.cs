26bfbadc6d909a1b4e77f805f23f6fdac0d3ec2e
huli <233237809@live.com>
2016-09-01 16:58:31 +0800

    新建项目

---
+++ AuthorsMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorsMS.Web.Startup))]
+namespace AuthorsMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
